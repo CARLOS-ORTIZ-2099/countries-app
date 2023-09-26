420fcdd1e45c17eeec45254d4a7e4cc4e8ad7f98
Carlos Eduardo Ortiz <carlosklinton.2016@gmail.com>
2023-09-25 20:03:47 -0500

    se modifico el archivo errorBusqueda

--- src/components/ErrorBusqueda.jsx
+++ src/components/ErrorBusqueda.jsx
@@ -4,16 +4,16 @@ import './error-busqueda.css'
 import logoError from '../images/loaders/error-page-svgrepo-com.svg'
 
 export const ErrorBusqueda = ({error}) => {
- 
+ console.log(error)
 
 
 
   return (
     <div className="continer">
-        <h1 className="title-error">Error De Busqueda</h1>
+      <h1 className="title-error">Error De Busqueda</h1>
         <h2 className="prop-error">{error}</h2>
         <hr/>
-        <img style={{width:'70%' , marginTop:'2rem',}} src={logoError} alt="" />
+        <img style={{width:'70%' , marginTop:'2rem',}} src={logoError} alt="" /> 
         
     </div>
   )
